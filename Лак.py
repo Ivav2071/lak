520221ca3839007d7f10198fe5af83306ea84c15
Ivav2071 <ivav_2071@mail.ru>
2024-07-02 03:43:36 +0700

    first

---
+++ Лак.py
@@ -0,0 +1,14 @@
+words = "всеМ прИвЕт, прИяТно ПоЗНакоМиТьсЯ!"
+
+words = words.capitalize()
+
+
+if "знак" in words:
+    words = words.replace("знак","лак")
+
+print(words)
+
+int(input("Нажмите Enter чтобы закрыть"))
+
+
+
